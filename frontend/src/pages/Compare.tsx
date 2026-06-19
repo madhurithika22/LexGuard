c1d0873175875ffa5bbadee1f73638b89686bc2c
Madhu Rithika <madhurithika22@gmail.com>
2026-06-19 22:21:53 +0530

    Update import statement for ComparisonResult type

--- frontend/src/pages/Compare.tsx
+++ frontend/src/pages/Compare.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useContractStore, ComparisonResult } from "../store/contractStore";
+import { useContractStore, type ComparisonResult } from "../store/contractStore";
 import { 
   Columns, 
   FileText, 
